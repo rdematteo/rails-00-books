d5884d7c9874145e42c3b998c51622c5bd85179f
Robert De Matteo <robertdematteo@hotmail.com>
2019-04-16 16:34:51 +1000

    changes made with ryan

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get "/books", to: "books#index", as: "books"
+  post "/books", to: "books#create"
+  get "/books/new", to: "books#new", as: "new_book"
+  get "/books/:id", to: "books#show", as: "book"
+  put "/books/:id", to: "books#update"
+  patch "/books/:id", to: "books#update"
+  get "/books/:id/edit", to: "books#edit", as: "edit_book"
+
+  delete '/books/:id', to: "books#destroy", as: "delete_book"
+  
   root to: 'books#index'
-  get '/books/:id', to: 'books#show'
 end
